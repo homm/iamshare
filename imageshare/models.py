31e45f8eb895ca46aadca3ea282144de8256bc6d
homm <homm86@gmail.com>
2013-04-23 20:03:24 +0400

    model slug generation

--- imageshare/models.py
+++ imageshare/models.py
@@ -1,7 +1,18 @@
+import string
+import random
+
 from pyuploadcare.dj import ImageField
 from django.db import models
 
 
 class Image(models.Model):
-    slug = models.SlugField(max_length=10, primary_key=True, blank=False)
+    slug = models.SlugField(max_length=10, primary_key=True)
     image = ImageField(manual_crop="")
+
+    def __repr__(self):
+        return u'<Image slug={0} image={1}>'.format(self.slug, self.image)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = ''.join(random.sample(string.ascii_lowercase, 6))
+        super(Image, self).save(*args, **kwargs)
