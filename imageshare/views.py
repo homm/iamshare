1e5bbec774bd4664d432d4c4b6c5ee68e0a8ca3a
homm <homm86@gmail.com>
2013-04-25 13:24:35 +0400

    make 404 not 500

--- imageshare/views.py
+++ imageshare/views.py
@@ -1,5 +1,7 @@
+from pyuploadcare import InvalidRequestError
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView
+from django.http import Http404
 
 from .models import Image
 
@@ -10,3 +12,14 @@ class UploadView(CreateView):
 
 class ImageView(DetailView):
     model = Image
+
+    def get_object(self):
+        object = super(ImageView, self).get_object()
+        try:
+            if object.image.is_removed:
+                raise ValueError('File was deleted.')
+        except (InvalidRequestError, ValueError):
+            object.delete()
+            raise Http404
+
+        return object
