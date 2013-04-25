ab3f66ea170d5af7a3d00af4f4b20c300fa5879f
homm <homm86@gmail.com>
2013-04-25 21:11:29 +0400

    local.py

--- upload_test/settings.py
+++ upload_test/settings.py
@@ -1,6 +1,6 @@
 # Django settings for upload_test project.
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -29,7 +29,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -155,3 +155,8 @@ LOGGING = {
         },
     }
 }
+
+try:
+    from .local import *
+except ImportError:
+    pass
