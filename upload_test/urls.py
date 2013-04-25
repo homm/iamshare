3b90d6ea801a048d904fb66dc0d54a3e03470940
homm <homm86@gmail.com>
2013-04-25 13:01:46 +0400

    link to index

--- upload_test/urls.py
+++ upload_test/urls.py
@@ -4,6 +4,6 @@ from imageshare import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', views.UploadView.as_view()),
+    url(r'^$', views.UploadView.as_view(), name='index'),
     url(r'^view/(?P<pk>[a-z]{6})/$', views.ImageView.as_view(), name='detail'),
 )
